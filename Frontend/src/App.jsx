19aec55978879c58a3bbaaac0b4731ed5a373694
ayanbhuyan700 <ayanbhuyan384@gmail.com>
2026-09-14 17:58:25 +0530

    add orders route to App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Register from "./containers/Register";
 import ViewProduct from "./containers/ViewProduct";
 import Categories from "./containers/Categories";
 import Cart from "./containers/Cart";
+import Orders from "./containers/Orders";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" element={<Orders />} />
           <Route path="/product/:id" element={<ViewProduct />} />
           <Route path="/categories/:category" element={<Categories />} />
         </Routes>
