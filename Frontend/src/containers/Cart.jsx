b1f4e70d8043a79dbce0aad3f1eb797ba6898fc7
ayanbhuyan700 <ayanbhuyan384@gmail.com>
2026-09-25 14:07:43 +0530

    fix: remove unnecessary character from "Continue Shopping" button in Cart component

--- Frontend/src/containers/Cart.jsx
+++ Frontend/src/containers/Cart.jsx
@@ -245,7 +245,7 @@ function Cart() {
                             onClick={() => navigate("/allProduct")}
                         >
                             <img className="group-hover:-translate-x-1 transition" alt="arrow" src="/images/greenarrow.svg" onError={(e) => { e.target.style.display = 'none'; }} />
-                            <span>←</span> Continue Shopping
+                             Continue Shopping
                         </button>
                     </div>
 
